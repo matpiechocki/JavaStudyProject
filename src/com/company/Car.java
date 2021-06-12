0163655d00abae41e3cf26d81fd090a68bed5f8b
matpiechocki <mateusz.piechocki@gmail.com>
2021-06-12 22:09:03 +0200

    Zadanie 2

---
+++ src/com/company/Car.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Car
+{
+    final String model;
+    final String producer;
+    final String color;
+    final String type;
+
+    public Car(String model, String producer, String color, String type)
+    {
+        this.model = model;
+        this.producer = producer;
+        this.color = color;
+        this.type = type;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,16 +9,39 @@ public class Main
         dog.name="Szarik";
 
         //System.out.println("DogName = "+dog.name);
-        //Phone iphone = new Phone("apple", "7","iOS", 4.7);
+        Phone iphone = new Phone("apple", "7","iOS", 4.7);
         /*
         System.out.println("Producer = "+iphone.producer);
         System.out.println("Model = "+iphone.model);
         System.out.println("OS = "+iphone.operationSystem);
         System.out.println("Screen Size = "+iphone.screenSize);
-        Human me = new Human();
-        me.mobilePhone = iphone;
         */
+        Human me = new Human();
+        me.firstName = "Mateusz";
+        me.lastName = "Piechocki";
+        me.age = "21";
+        me.sex = "M";
+        me.mobilePhone = iphone;
+        /*
+        //Test Phone -> Human
+        System.out.println(me.firstName+" "+me.lastName+",PHONE: "
+                +me.mobilePhone.producer+"-"+me.mobilePhone.model+"-"+me.mobilePhone.operationSystem+"-"+me.mobilePhone.screenSize);
+        */
+        Car firstCar = new Car("Enyaq iV", "Skoda", "white", "SUV");
+        //Test firstCar
+        System.out.println("Model: "+firstCar.model);
+        System.out.println("Producer: "+firstCar.producer);
+        System.out.println("Color: "+firstCar.color);
+        System.out.println("Type: "+firstCar.type);
 
+        me.fCar = firstCar;
+
+        //Test Car -> Human
+        System.out.println(me.firstName+" "+me.lastName+",CAR: "
+                +me.fCar.producer+"-"+me.fCar.model+"-"+me.fCar.type+"-"+me.fCar.color);
+
+        /*
+        //Test - feed(), takeForAWalk()
         dog.feed();
         dog.takeForAWalk();
         dog.feed();
@@ -49,5 +72,6 @@ public class Main
         dog.takeForAWalk();
         dog.takeForAWalk();
         dog.feed();
+        */
     }
 }
